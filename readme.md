2afe50ba5cb515100826ca54b815481221d18347
Clarissa Jaime <clarissanjaime@gmail.com>
2017-07-30 13:02:04 -0400

    Setup mongodb, added post functionality

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Blogapp
+
+A blog application.
+
+# Reqs
+
+[x] User can see list of blog posts
+[x] Navigate to blog detail
+[ ] Add new blog post
+[ ] Edit blog post
+[ ] Delete blog post
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,64 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient
+var ObjectID = require('mongodb').ObjectID
 
-posts = [
-  {
-    id: 1,
-    title: "First blog post",
-    content: "first post content"
-  },
-  {
-    id: 2,
-    title: "Second blog post",
-    content: "second post content"
-  },
-  {
-    id: 3,
-    title: "third blog post",
-    content: 'third post content'
-  },
-];
+// posts = [
+//   {
+//     id: 1,
+//     title: "First blog post",
+//     content: "first post content"
+//   },
+//   {
+//     id: 2,
+//     title: "Second blog post",
+//     content: "second post content"
+//   },
+//   {
+//     id: 3,
+//     title: "third blog post",
+//     content: 'third post content'
+//   }
+// ];
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { posts: posts });
+  MongoClient.connect('mongodb://localhost:27017/blogapp', function(err, db) {
+    if (err) console.log(err);
+
+    db.collection('blogposts').find().toArray(function (err, result) {
+      res.render('index', { posts: result });
+    });
+  });
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  // Save the post
-  // Get an id for that post
-  // Redirect to /post/id
   var title = req.body.title;
   var content = req.body.body;
-  res.render('post', { title: title, content: content });
+
+
+  MongoClient.connect('mongodb://localhost:27017/blogapp', function(err, db) {
+    if (err) throw err;
+
+    db.collection('blogposts').insertOne({
+      title: title,
+      content: content
+    }, function(err, result) {
+      res.redirect('/post/' + result.insertedId);
+    })
+  });
 });
 
 router.get('/post/:id', function(req, res, next) {
-  var post = posts.find(function(post) {
-    return post.id == req.params.id;
+  var id = new ObjectID(req.params.id);
+
+  MongoClient.connect('mongodb://localhost:27017/blogapp', function(err, db) {
+    if (err) throw err;
+
+    db.collection('blogposts').findOne({"_id": id}, function(err, result){
+      console.log(result);
+      res.render('post', result);
+    });
   });
-  res.render('post', post);
 });
 
 router.get('/new', function(req, res, next) {
